8e43a3641788ec4c2e6940304c3087a10f3f5fa3
mp2125 <mp2125@cam.ac.uk>
2026-05-29 14:31:34 +0100

    changed folder

--- other_groups.py
+++ other_groups.py
@@ -1,6 +1,7 @@
 from ShellAndTubeHeatExchanger import ShellAndTubeHeatExchanger as HX
 from optimiser import fullSolver
 from weight_limits import getWeight
+from pprint import pprint
 
 
 group_d1 = HX(10, 11, 0.243, tube_pitch=0.02, is_square_layout=False, tube_passes=2, shell_passes=2)
@@ -11,17 +12,24 @@ group_c = HX(12, 7, 0.230, tube_pitch=0.012, is_square_layout=False, tube_passes
 group_e = HX(16, 8, 0.185, is_square_layout=False, tube_passes=2, shell_passes=1)
 
 other_hxs = [
-    group_a, group_b, group_c, group_d2, group_e
+    group_a, group_b, group_c, group_d1, group_e
 ]
 
 predicted_transfers = [fullSolver(group)[1] for group in other_hxs]
 weights = [getWeight(group) for group in other_hxs]
 
 if __name__ == '__main__':
-    for prediction in predicted_transfers:
-        print(f'{prediction:.0f} W')
-
-    for weight in weights:
-        print(weight)
+    for hx, prediction, group in zip(other_hxs, predicted_transfers, "ABCDE"):
+        pprint(", ".join(map(str,[
+            group,
+            hx.number_of_tubes,
+            hx.number_of_baffles,
+            hx.tube_length,
+            float(hx.tube_pitch),
+            hx.tube_passes,
+            hx.shell_passes,
+            getWeight(hx),
+            f'{prediction:.0f}',
+            ])))
 
 
